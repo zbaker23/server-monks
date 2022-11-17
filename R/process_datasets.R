6fc6ea8520e87b97402303780d072d5bf03b717e
Zoe Baker <zbaker@smith.edu>
2022-11-17 10:07:08 -0500

    added process_dataset, though still need to add final edits to documentation, made minor edits to data-raw with clean_names()

---
+++ R/process_datasets.R
@@ -0,0 +1,32 @@
+#' California county vaccination data
+#'
+#' A subset of data from the California Department of Health ...
+#'
+#' @format ## `vaxx_counties`
+#' A data frame with 52,204 entries and 20 columns:
+#' \describe{
+#'   \item{county}{Country name}
+#'   \item{administered_date, iso3}{2 & 3 letter ISO country codes}
+#'   \item{total_doses}{Year}
+#'   \item{cumulative_total_doses}{Country name}
+#'   \item{pfizer_doses}{2 & 3 letter ISO country codes}
+#'   \item{cumulative_pfizer_doses}{Year}
+#'   \item{moderna_doses}{Country name}
+#'   \item{cumulative_moderna_doses}{2 & 3 letter ISO country codes}
+#'   \item{jj_doses}{Year}
+#'   \item{countr}{Country name}
+#'   \item{cumulative_jj_doses}{2 & 3 letter ISO country codes}
+#'   \item{partially_vaccinated}{Year}
+#'   \item{total_partially_vaccinated}{Country name}
+#'   \item{fully_vaccinated}{2 & 3 letter ISO country codes}
+#'   \item{cumulative_fully_vaccinated}{Year}
+#'   \item{at_least_one_dose}{Country name}
+#'   \item{cumukative_at_least_one_dose}{2 & 3 letter ISO country codes}
+#'   \item{california_flag}{Year}
+#'   \item{booster_recip_count}{Country name}
+#'   \item{cumulative_booster_recip_count}{2 & 3 letter ISO country codes}
+#'   \item{booster_eligible_population}{Year}
+#'   ...
+#' }
+#' @source <https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv>
+"California Department of Health"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
 library(tidyverse)
-vaxx_counties <- read_csv("data-raw/covid19vaccinesbycounty.csv")
+vaxx_counties <- read_csv("data-raw/covid19vaccinesbycounty.csv") %>%
+  clean_names()
 usethis::use_data(DATASET, overwrite = TRUE)
