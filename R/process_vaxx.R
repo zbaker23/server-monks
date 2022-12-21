dd54b57c34155e671a3dff0b58ee9885b2ee7776
Zoe Baker <zbaker@smith.edu>
2022-12-21 04:33:07 -0500

    CMD check pass!!!

--- R/process_vaxx.R
+++ R/process_vaxx.R
@@ -31,3 +31,4 @@
 #'
 #' @source <https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv>
 "vaxx_counties"
+
